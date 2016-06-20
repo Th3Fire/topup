8c21b94efb6e2e0cab0b4a9f960d04d77670dc62
Wuttinunt Chaoyod <wuttinunt@users.noreply.github.com>
2016-06-20 15:20:59 +0700

    Update connect_db.php

--- connect_db.php
+++ connect_db.php
@@ -11,8 +11,5 @@
 		echo "DataBase connect Failed: " .mysqli_connect_error();
 		exit();
 	}
-	//***** Auto logout when user AFK
-	//$intRejectTime = 20;
-	//$sql = "UPDATE member SET LoginStatus = '0', LasteUpdate = '0000-00-00 00:00:00' WHERE 1 AND date_add(LasteUpdate,interval $intRejectTime MINUTE) <= NOW() ";
-	//$query = mysqli_query($con,$sql);
+	
 ?>
